1a6fc30d5434beaf0f7e6eaa818301119135face
Jonas Kvinge <jonas@jkvinge.net>
2023-07-17 21:47:36 +0200

    Use mimalloc

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,10 @@
 
 #include <glib.h>
 
+#ifdef HAVE_MIMALLOC
+#  include <mimalloc.h>
+#endif
+
 #include <QObject>
 #include <QApplication>
 #include <QCoreApplication>
@@ -119,6 +123,10 @@
 using std::make_shared;
 
 int main(int argc, char *argv[]) {
+
+#ifdef HAVE_MIMALLOC
+  mi_version();
+#endif
 
 #ifdef Q_OS_MACOS
   // Do Mac specific startup to get media keys working.
